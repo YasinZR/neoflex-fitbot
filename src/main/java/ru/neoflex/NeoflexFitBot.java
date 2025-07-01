2717a13dc3fad1fbd62b9c8be06d0692ad844845
Master.OK <s_k_i_l_l.2004@bk.ru>
2025-07-01 23:51:07 +0400

    Добавил 5 inline кнопок после привествия с пользователем

--- src/main/java/ru/neoflex/NeoflexFitBot.java
+++ src/main/java/ru/neoflex/NeoflexFitBot.java
@@ -1,5 +1,9 @@
 package ru.neoflex;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import ru.neoflex.config.BotConfig;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -33,10 +37,39 @@ public class NeoflexFitBot  extends TelegramLongPollingBot {
             String userName = update.getMessage().getFrom().getFirstName();
 
             if (messageText.equals("/start")) {
-                String reply = "Привет, " + userName + "! 👋\nЯ твой персональный помощник по здоровью и спорту.\n" +
-                        "Вместе мы сделаем тебя сильнее, выносливее и организованнее!";
-                sendMessage(chatId, reply);
+                sendMainMenu(chatId, userName);
+//                String reply = "Привет, " + userName + "! 👋\nЯ твой персональный помощник по здоровью и спорту.\n" +
+//                        "Вместе мы сделаем тебя сильнее, выносливее и организованнее!";
+//                sendMessage(chatId, reply);
             }
+        }
+    }
+
+    private void sendMainMenu(long chatId, String userName) {
+        String text = "Привет, " + userName + "! Я твой персональный помощник по здоровью и спорту. \n " +
+                "Вместе мы сделаем тебя сильнее, выносливее и организованнее!\nВыбери, что тебя интересует:";
+
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+
+        rows.add(List.of(InlineKeyboardButton.builder().text("💪 Привычки").callbackData("habits").build()));
+        rows.add(List.of(InlineKeyboardButton.builder().text("🧮 БЖУ").callbackData("bju").build()));
+        rows.add(List.of(InlineKeyboardButton.builder().text("💧 Вода").callbackData("water").build()));
+        rows.add(List.of(InlineKeyboardButton.builder().text("📊 Статистика").callbackData("stats").build()));
+        rows.add(List.of(InlineKeyboardButton.builder().text("⚙️ Настройки").callbackData("settings").build()));
+
+        markup.setKeyboard(rows);
+
+        SendMessage message = SendMessage.builder()
+                .chatId(String.valueOf(chatId))
+                .text(text)
+                .replyMarkup(markup)
+                .build();
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
     }
 
